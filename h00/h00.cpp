e5af1370f541e9f984b80143e5071a9d7ae741c2
Zdenek Bocek <zbocek@student.cccd.edu>
2021-02-04 02:14:13 +0000

    auto commit 02/04/21 02:14:13

--- h00/h00.cpp
+++ h00/h00.cpp
@@ -4,6 +4,7 @@
  *  @file h00.cpp
  */
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
@@ -16,11 +17,21 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    // Add your implementation comments here
-
-    cout << STUDENT << " starter code." << endl;
-    cout << "[1, 1]" << endl;
-
+    // Input: weight of box of cereal in ounces
+    // Output: weight of box in metric tons, boxes per ton
+    // Given: Metric ton is 35,273.92 ounces
+    // Calculation: weight in tons->weight divided by metric tons per ounce
+    // Calculation: boxes->1 divided by weight in tons
+    const double metTon{35273.92};
+    cout << STUDENT << "-" << ASSIGNMENT << ": ";
+    cout << "Cereal Box Calculator" << endl;
+    cout << string(50, '-') << endl;
+    cout << "Enter ounces per box of cereal: ";
+    double cerBox;
+    cin >> cerBox;
+    double weightMetTons = cerBox / metTon;
+    double boxesPerTon = metTon / cerBox;
+    cout << "Weight in Metric tons, boxes per ton: [" << weightMetTons << ", " << boxesPerTon << "]" << endl;
 
     return 0;
 }
